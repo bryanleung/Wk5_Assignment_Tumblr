ed88d8594212c81fe314954a6d16a6dc63d7aed6
Bryan Leung <brleung@zynga.com>
2015-03-07 23:36:55 -0800

    Initial Commit

---
+++ Wk5_Assignment_Tumblr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wk5_Assignment_Tumblr
+//
+//  Created by Bryan Leung on 3/7/15.
+//  Copyright (c) 2015 Bryan Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
