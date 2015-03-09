9c0432a7bd1240b6b01107900f9caa0c8b967ded
Bryan Leung <brleung@zynga.com>
2015-03-08 22:28:04 -0700

    added loading animation to search feed

--- Wk5_Assignment_Tumblr/SearchViewController.swift
+++ Wk5_Assignment_Tumblr/SearchViewController.swift
@@ -11,13 +11,25 @@ import UIKit
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loadingImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.contentSize = CGSize(width: 320, height: 1217)
         scrollView.contentInset.bottom = 44
+        scrollView.hidden = true
+        scrollView.alpha = 0
+        loadingImage.hidden = false
+        
+        var images = UIImage.animatedImageNamed("loading-", duration: 0.7)
+        loadingImage.image = images
 
         // Do any additional setup after loading the view.
+
+
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +37,23 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool){
+        delay(2, { () -> () in
+            UIView.animateWithDuration(
+                0.3, animations: { () -> Void in
+                    self.scrollView.alpha = 1
+                    self.loadingImage.alpha = 0
+                    
+            })
+            self.scrollView.hidden = false
+            self.loadingImage.hidden = true
+        })
     }
-    */
-
+    
+    override func viewWillDisappear(animated: Bool) {
+        scrollView.hidden = true
+        loadingImage.hidden = false
+        scrollView.alpha = 0
+        loadingImage.alpha = 1
+    }
 }
